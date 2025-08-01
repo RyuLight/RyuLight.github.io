16573c68e9c68670dbe25e5e0df09421b7acf032
Unkleeable <42614674+RyuLight@users.noreply.github.com>
2025-08-02 06:17:24 +0800

    WALLAHI DESKTOP

--- main.js
+++ main.js
@@ -5,51 +5,71 @@
 document.addEventListener("click", function() 
 {
     let bgm = document.getElementById('bgm');
+    bgm.volume = 0.2;
     bgm.play();
     bgm.muted = false;
 }, {once: true});
 
-// TESTING SHIT BELOW
-    const textHover = document.querySelector("#tips1");
-    const test = document.querySelector("#test");
-    textHover.onmouseover = function ()
-    {
-        test.style.display = "block";
-    };
-
-    textHover.onmouseout = function ()
-    {
-        test.style.display = "none";
-    };
-
-    document.addEventListener("mousemove", function(e){
-        test.style.top = (e.pageY - 300)+ "px";
-        test.style.left = (e.pageX - 175) + "px";
-    });
-
-
-
-    // TESTING SHIT ABOVE
-
 document.addEventListener("DOMContentLoaded", function(){
     const allButtons = document.querySelectorAll("#menu button[data-id]");
     const pages = document.querySelectorAll("main .page");
-
+    
     const startPage = document.querySelector("#startPage");
     const mutebtn = document.querySelector("#mute");
     const closebtn = document.querySelector("#closeMenu");
     const menu = document.querySelector("#menu");
-    const funnybtn = document.querySelector("#funnybtn");
+    
+    // Don't bother with this, just for fun
+    {
+        const funnybtn = document.querySelector("#funnybtn");
     funnybtn.style.display = "none";
     setTimeout(function(){
         funnybtn.style.display = "inline";
     }, 20000);
-
+    }
 
     var isOpen = true;
-
-
     
+    // Text to hover on
+    // const tips1Hover = document.querySelector("span#tips1");
+    // const tips2Hover = document.querySelector("span#tips2");
+    const allTips = document.querySelectorAll("span[tips-id]");
+
+    allTips.forEach(tips => {
+        const tooltip = document.getElementById(tips.getAttribute("tips-id"));
+
+        tips.onmouseover = function() {
+            tooltip.style.display = "block";
+        };
+
+        tips.onmouseout = function() {
+            tooltip.style.display = "none";
+        };
+
+        document.addEventListener("mousemove", function(e){
+            tooltip.style.top = (e.pageY - tooltip.style.height)+ "px";
+            tooltip.style.left = (e.pageX - (tooltip.style.width - 20)) + "px";
+        });
+    });
+    
+
+    /* tips1Hover.onmouseover = function ()
+    {
+        tips1.style.display = "block";
+    };
+
+    tips1Hover.onmouseout = function ()
+    {
+        tips1.style.display = "none";
+    };
+
+    document.addEventListener("mousemove", function(e){
+        tips1.style.top = (e.pageY - tips1.style.height)+ "px";
+        tips1.style.left = (e.pageX - tips1.style.width) + "px";
+    }); */
+
+    // TESTING SHIT ABOVE
+
 
     mutebtn.style.backgroundColor = "white";
     closebtn.style.backgroundColor = "white";
@@ -97,6 +117,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
             pages.forEach(section => {
                 section.style.display="none";
+                if (section.classList.contains("moveAnim"))
+                {
+                    section.classList.toggle("moveAnim");
+                }
             });
 
             const selectedPage = document.getElementById(selectedBtn);
@@ -128,3 +152,206 @@ document.addEventListener("DOMContentLoaded", function(){
             menu.classList.toggle("panelAnim");
     }
 });
+
+// Sheep Game
+const sheep1 = document.querySelector("#sheep1");
+const sheep2 = document.querySelector("#sheep2");
+const sheep3 = document.querySelector("#sheep3");
+const startGame = document.querySelector("#startGame");
+var gameStart = false;
+var gameEnd = false;
+var sheepCount = 0;
+var maxSheep = 0;
+
+// Quiz Related
+const answer1 = document.querySelector("#answer1");
+const answer2 = document.querySelector("#answer2");
+const answer3 = document.querySelector("#answer3");
+const answer4 = document.querySelector("#answer4");
+const answers = [answer1, answer2, answer3, answer4];
+const restart = document.querySelector("#restart");
+const quiz = document.querySelector("#quiz");
+
+restart.addEventListener("click", function(){
+    resetSheepGame();
+});
+
+function resetSheepGame() {
+    restart.style.display = "none";
+    answers.forEach(answer => {
+        answer.style.display = "inline";
+    });
+    quiz.style.display = "none";
+    // maxSheep = Math.floor(Math.random() * (51 - 10) + 10);
+    maxSheep = 10;
+    sheepCount = maxSheep;
+    sheep1.style.transform = 'translate(1500px, 50px)';
+    sheep2.style.transform = 'translate(1500px, 300px)';
+    sheep3.style.transform = 'translate(1500px, 550px)';
+    sheep1.style.display = 'inline';
+    sheep2.style.display = 'inline';
+    sheep3.style.display = 'inline';
+    for (let i = 1; i < 4; i++)
+    {
+        resetPosAndRand(i);
+    }
+    RandomizeAnswer();
+    if (gameStart)
+    {
+        let tutorial = document.querySelector("#tutorial");
+        tutorial.style.display = "block";
+    }
+    gameStart = false;
+    gameEnd = false;
+};
+
+startGame.addEventListener("click", function(){
+    let tutorial = document.querySelector("#tutorial");
+    tutorial.style.display = "none";
+    resetSheepGame();
+    gameStart = true;
+});
+
+var newX1 = 0;
+var newX2 = 0;
+var newX3 = 0;
+var moveSpeed1 = Math.random() * (30 - 5) + 5;
+var moveSpeed2 = Math.random() * (30 - 5) + 5;
+var moveSpeed3 = Math.random() * (30 - 5) + 5;
+
+
+function resetPosAndRand(number){
+    
+    console.log(sheepCount);
+    switch (number)
+    {
+        case 1:
+            newX1 = 0;
+            if (sheepCount <= 3)
+            {
+                console.log("SHEEP 1 NO SPAWN");
+                sheep1.style.left = 0;
+                moveSpeed1 = 0;
+                sheep1.style.display = "none";
+            }
+            else
+            {
+                console.log("SHEEP 1 SPAWNED LOL");
+                sheep1.style.left = 0;
+                moveSpeed1 = Math.random() * (30 - 5) + 5;
+            }
+            break;
+        case 2:
+            newX2 = 0;
+            if (sheepCount <= 3)
+            {
+                sheep2.style.left = 0;
+                moveSpeed2 = 0;
+                sheep2.style.display = "none";
+            }
+            else
+            {
+                sheep2.style.left = 0;
+                moveSpeed2 = Math.random() * (30 - 5) + 5;
+            }
+            break;
+        case 3:
+            newX3 = 0;
+            if (sheepCount <= 3)
+            {
+                sheep3.style.left = 0;
+                moveSpeed3 = 0;
+                sheep3.style.display = "none";
+            }
+            else
+            {
+                sheep3.style.left = 0;
+                moveSpeed3 = Math.random() * (30 - 5) + 5;
+            }
+            break;
+    }
+}
+
+let gamePlay = setInterval(function(){
+    if (gameStart && !gameEnd)
+    {
+        newX1 -= moveSpeed1;
+        newX2 -= moveSpeed2;
+        newX3 -= moveSpeed3;
+
+        sheep1.style.left = newX1 + "px";
+        sheep2.style.left = newX2 + "px";
+        sheep3.style.left = newX3 + "px";
+
+        if (sheepCount <= 1)
+        {
+            gameEnd = true;
+        }
+
+        if (newX1 <= -1700)
+        {
+            resetPosAndRand(1);
+            sheepCount--;
+        }
+        if (newX2 <= -1700)
+        {
+            resetPosAndRand(2);
+            sheepCount--;
+        }
+        if (newX3 <= -1700)
+        {
+            resetPosAndRand(3);
+            sheepCount--;
+        }
+    }
+    else if (gameStart && gameEnd)
+    {
+        quiz.style.display = "block";
+
+        answers.forEach(answer => {
+            answer.addEventListener("click", function(){
+                if (answer.textContent == maxSheep)
+                {
+                    let words = document.querySelector("#topText");
+                    words.textContent = "Correct! You counted and remembered well, remember to take care of yourself and have a good sleep!";
+                    answers.forEach(allAnswer => {
+                        allAnswer.style.display = "none";
+                    });
+                    restart.style.display = "inline";
+                }
+                else
+                {
+                    let words = document.querySelector("#topText");
+                    words.textContent = "Wrong! The correct answer was " + maxSheep + " Try again by pressing the button below!";
+                    answers.forEach(allAnswer => {
+                        allAnswer.style.display = "none";
+                    });
+                    restart.style.display = "inline";
+                }
+            });
+        });
+    }
+}, 25);
+
+function RandomizeAnswer(){
+    answer1.textContent = Math.floor((Math.random() * maxSheep) + 5);
+    answer2.textContent = Math.floor((Math.random() * maxSheep) + 5);
+    answer3.textContent = Math.floor((Math.random() * maxSheep) + 5);
+    answer4.textContent = Math.floor((Math.random() * maxSheep) + 5);
+
+    answers.forEach(answer => {
+        // ENSURE THE ANSWER WILL NEVER BE THE SAME
+        // OR I HOPE IT DOES ATLEAST
+        if (answer.textContent == maxSheep)
+        {
+            answer.textContent = Math.floor((Math.random() * maxSheep) + 5);
+        }
+    });
+
+
+    var rightAnswer = Math.floor((Math.random() * 4) + 1);
+
+    let chosenButton = document.getElementById(`answer${rightAnswer}`);
+    chosenButton.textContent = maxSheep;
+    console.log(chosenButton);
+}
